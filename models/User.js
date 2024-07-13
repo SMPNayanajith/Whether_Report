627c88e6091f47eda534c5396f740c4bd5120486
SMPNayanajith <pasindusamarakoon500@gmail.com>
2024-07-13 22:56:30 +0530

    user

--- models/User.js
+++ models/User.js
@@ -1,16 +1,28 @@
-const weatherDataSchema = new mongoose.Schema({
-    date: { type: String, required: true },
-    time: { type: String, required: true },
-    weather: { type: String, required: true },
-});
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     location: { type: String, required: true },
-    weatherData: [weatherDataSchema],
+    weatherData: [
+        {
+            date: Date,
+            weather: String,
+        }
+    ]
 });
 
-const User = mongoose.model('User', userSchema);
+// Password hash middleware
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
-module.exports = User;
+// Password verification method
+userSchema.methods.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
